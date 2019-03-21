8bff4838d985cc778095d11abf8d46070888bc43
Christian Payer <christian.payer@gmx.net>
2019-03-21 10:08:06 +0100

    fixed reorient script

--- bin/experiments/semantic_segmentation/mmwhs/reorient_prediction_to_reference.py
+++ bin/experiments/semantic_segmentation/mmwhs/reorient_prediction_to_reference.py
@@ -44,7 +44,9 @@ if __name__ == '__main__':
     is_ct = False
     # TODO: change folder to where the predictions are saved
     input_folder = 'TODO'
-    reference_folder = './mmwhs_dataset/ct_mha/' if is_ct else './mmwhs_dataset/mr_mha/'
+    # TODO: change folder to where original files (e.g. mr_train_1001_image.nii.gz and mr_train_1001_label.nii.gz) from MMWHS challenge are saved
+    reference_folder = 'TODO' if is_ct else 'TODO'
+
     output_folder = './reoriented/ct/' if is_ct else './reoriented/mr/'
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -56,7 +58,7 @@ if __name__ == '__main__':
         basename_wo_ext = basename[:basename.find('.mha')]
         print(basename_wo_ext)
         image = itk.imread(filename)
-        reference = itk.imread(os.path.join(reference_folder, basename_wo_ext + '_image.mha'))
+        reference = itk.imread(os.path.join(reference_folder, basename_wo_ext + '_label.nii.gz'))
         reoriented = cast(image, reference)
         reoriented = reorient_to_reference(reoriented, reference)
         reoriented = copy_information(reoriented, reference)
