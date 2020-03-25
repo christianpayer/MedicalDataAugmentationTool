734623d4b589cc2878a1ccd5b51e1ad1d740b38a
Christian Payer <christian.payer@gmx.net>
2020-03-25 13:16:19 +0100

    add segmentation metrics

--- utils/segmentation/metrics.py
+++ utils/segmentation/metrics.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import utils.sitk_np
+import utils.sitk_image
 
 class MetricBase(object):
     def __call__(self, prediction_sitk, groundtruth_sitk, labels):
@@ -14,38 +15,59 @@ class TpFpFnMetricBase(MetricBase):
         fn = np.sum(np.logical_and(prediction_np != label, groundtruth_np == label))
         return tp, fp, fn
 
-    def calculate_tp_fp_fn_scores(self, prediction_sitk, groundtruth_sitk, labels):
+    def calculate_tp_fp_fn_lists(self, prediction_sitk, groundtruth_sitk, labels):
         prediction_np = utils.sitk_np.sitk_to_np_no_copy(prediction_sitk)
         groundtruth_np = utils.sitk_np.sitk_to_np_no_copy(groundtruth_sitk)
-        scores = []
-        for label in labels:
-            tp, fp, fn = self.calculate_tp_fp_fn(prediction_np, groundtruth_np, label)
-            current_score = self.evaluate_function(tp, fp, fn)
-            scores.append(current_score)
-        return scores
-
-    def __call__(self, prediction_sitk, groundtruth_sitk, labels):
-        return self.calculate_tp_fp_fn_scores(prediction_sitk, groundtruth_sitk, labels)
+        return [self.calculate_tp_fp_fn(prediction_np, groundtruth_np, label) for label in labels]
 
     def evaluate_function(self, tp, fp, fn):
         raise NotImplementedError()
 
 
-class DiceMetric(TpFpFnMetricBase):
+class TpFpFnMetricPerLabel(TpFpFnMetricBase):
+    def __call__(self, prediction_sitk, groundtruth_sitk, labels):
+        tp_fp_fn_list = self.calculate_tp_fp_fn_lists(prediction_sitk, groundtruth_sitk, labels)
+        return [self.evaluate_function(tp, fp, fn) for tp, fp, fn in tp_fp_fn_list]
+
+
+class TpFpFnMetricGeneralized(TpFpFnMetricBase):
+    def __call__(self, prediction_sitk, groundtruth_sitk, labels):
+        tp_fp_fn_list = self.calculate_tp_fp_fn_lists(prediction_sitk, groundtruth_sitk, labels)
+        tp, fp, fn = [sum(i) for i in zip(*tp_fp_fn_list)]
+        return [self.evaluate_function(tp, fp, fn)]
+
+
+class DiceMetric(TpFpFnMetricPerLabel):
     def evaluate_function(self, tp, fp, fn):
-        return 2 * tp / (2 * tp + fp + fn) if tp + fp + fn > 0 else 1
+        return 2 * tp / (2 * tp + fp + fn) if tp + fn > 0 else np.nan
 
 
-class JaccardMetric(TpFpFnMetricBase):
+class JaccardMetric(TpFpFnMetricPerLabel):
     def evaluate_function(self, tp, fp, fn):
-        return tp / (tp + fp + fn) if tp + fp + fn > 0 else 1
+        return tp / (tp + fp + fn) if tp + fn > 0 else np.nan
 
 
-class SpecificitySensitivityMetric(TpFpFnMetricBase):
+class SpecificitySensitivityMetric(TpFpFnMetricPerLabel):
     def evaluate_function(self, tp, fp, fn):
-        return tp / (tp + fn) if tp + fn > 0 else 1
+        return tp / (tp + fn) if tp + fn > 0 else np.nan
 
 
-class NegativePositivePredictiveValueMetric(TpFpFnMetricBase):
+class NegativePositivePredictiveValueMetric(TpFpFnMetricPerLabel):
     def evaluate_function(self, tp, fp, fn):
-        return tp / (tp + fp) if tp + fp > 0 else 1
+        return tp / (tp + fp) if tp + fn > 0 else np.nan
+
+
+class GeneralizedDiceMetric(TpFpFnMetricGeneralized):
+    def evaluate_function(self, tp, fp, fn):
+        return 2 * tp / (2 * tp + fp + fn) if tp + fn > 0 else np.nan
+
+
+class HausdorffDistanceMetric(MetricBase):
+    def __call__(self, prediction_sitk, groundtruth_sitk, labels):
+        return utils.sitk_image.hausdorff_distances(prediction_sitk, groundtruth_sitk, labels)
+
+class SurfaceDistanceMetric(MetricBase):
+    def __call__(self, prediction_sitk, groundtruth_sitk, labels):
+        return utils.sitk_image.surface_distances(prediction_sitk, groundtruth_sitk, labels)
+
+
